d114a374c975805175891808067d0c61897c7b6f
+ <escobedo.jose48@outlook.com>
2022-07-18 19:42:52 -0700

    app padding css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
+import styled from "styled-components";
 
 function App() {
   return (
-    <>
+    <AppConatiner>
       <Navbar />
-      <Home />
-    </>
+      <div className="container">
+        <Home />
+      </div>
+    </AppConatiner>
   );
 }
 
+const AppConatiner = styled.div`
+  .container {
+    display: flex;
+    margin-top: 10px;
+  }
+`;
 export default App;
