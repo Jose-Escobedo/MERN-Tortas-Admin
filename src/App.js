157d2c50b8cf969b58cff4722563bdb90d58cc8d
+ <escobedo.jose48@outlook.com>
2022-07-18 19:52:24 -0700

    Sidebar component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import styled from "styled-components";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <AppConatiner>
       <Navbar />
       <div className="container">
+        <Sidebar />
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>
@@ -20,6 +22,10 @@ const AppConatiner = styled.div`
   .container {
     display: flex;
     margin-top: 10px;
+  }
+  .link {
+    text-decoration: none;
+    color: inherit;
   }
 `;
 export default App;
