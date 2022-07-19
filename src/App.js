b5440706bd1784d838322d92132efeb4383bb855
+ <escobedo.jose48@outlook.com>
2022-07-18 19:37:30 -0700

    Navbar component added to home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
